b40416f975ee2be4723ccbc013c9a71baa03c390
Diego Fernando Velesaca Orellana <diegovo@outlook.com>
2021-09-22 17:46:17 -0500

    add Article model and setup the MySQL database engine

--- crud/mainapp/models.py
+++ crud/mainapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=150, verbose_name="Title")
+    content = models.TextField(verbose_name="Content")
+    image = models.ImageField(default='null', verbose_name="Image", upload_to="images/")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Creado")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Editado")
+
+    class Meta:
+        db_table = "data_articles"
+        verbose_name = "Article"
+        verbose_name_plural = "Articles"
+        ordering = ["-id"]
